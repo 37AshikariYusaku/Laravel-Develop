70d3d8ba59c15d56b4acadef1a76ee959e15aee0
Ashikari <12lependu24@gmail.com>
2021-01-31 09:13:34 +0000

    Develop

---
+++ resources/views/admin/review/create.blade.php
@@ -0,0 +1,27 @@
+<!--<!DOCTYPE html>-->
+<!--<html>-->
+<!--    <head>-->
+<!--        <meta charset="utf-8">-->
+<!--        <meta http-equiv="X-UA-Compatible" content="IE=edge">-->
+<!--        <meta name="viewport" content="width=device-width, initial-scale=1">-->
+
+<!--        <title>MyNews</title>-->
+<!--    </head>-->
+<!--    <body>-->
+<!--        <h1>Myニュース作成画面</h1>-->
+<!--    </body>-->
+<!--</html>-->
+
+@extends('layouts.admin')
+
+@section('title', 'レビュー投稿')
+
+@section('content')
+    <div class="container">
+        <duv class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>レビュー投稿</h2>
+            </div>
+        </duv>
+    </div>
+@endsection    
